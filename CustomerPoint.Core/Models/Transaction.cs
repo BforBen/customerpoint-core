8e000ba637c4adf318cec417492bd2bb834d2178
Ben <BforBen@users.noreply.github.com>
2016-03-31 14:24:06 +0100

    Update to new Payments API and provide client.

--- CustomerPoint.Core/Models/Transaction.cs
+++ CustomerPoint.Core/Models/Transaction.cs
@@ -1,49 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace CustomerPoint.Models
 {
-    [Obsolete("Temporary fix", false)]
     public class Transaction
     {
         /// <summary>
         /// Transaction reference number
         /// </summary>
-        public string Reference { get; set; }
+        public string Id { get; set; }
         /// <summary>
         /// Transaction date
         /// </summary>
         [DisplayFormat(DataFormatString = "{0:dd MMM yyyy HH:mm}")]
         public DateTime Date { get; set; }
-        /// <summary>
-        /// Date transcation was recorded
-        /// </summary>
-        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy HH:mm}")]
-        public DateTime Recorded { get; set; }
-        public string Account { get; set; }
+
+        public IEnumerable<string> Account { get; set; }
         public string Description { get; set; }
 
-        public string Fund { get; set; }
+        public string LedgerCode { get; set; }
         public string Method { get; set; }
         public string Channel { get; set; }
 
         [DataType(DataType.Currency)]
         public decimal? Amount { get; set; }
         [DataType(DataType.Currency)]
-        public decimal? TransactionCharge { get; set; }
-        [DataType(DataType.Currency)]
-        public decimal? TotalAmount { get; set; }
-
-        [DataType(DataType.Currency)]
         public decimal? Vat { get; set; }
-
-        public string CardType { get; set; }
-        public string CardCreditDebit { get; set; }
-
-        public string Name { get; set; }
-
         public string User { get; set; }
-
-        public string OriginalReference { get; set; }
     }
 }
---
+++ CustomerPoint.Core/Payments/Lookup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerPoint.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace CustomerPoint.Payments
+{
+    public static class Lookup
+    {
+        public static async Task<IEnumerable<Transaction>> Payments(string id = null, IEnumerable<string> reference = null, string fund = null, string ledgerCode = null, DateTime? from = null, DateTime? to = null, IEnumerable<string> methods = null, IEnumerable<string> xmethods = null)
+        {
+            using (var hc = new HttpClient())
+            {
+                hc.BaseAddress = Properties.Settings.Default.PaymentsServiceUri;
+                hc.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                List<string> QueryString = new List<string>();
+
+                if (!string.IsNullOrWhiteSpace(id))
+                {
+                    QueryString.Add("id=" + id);
+                }
+                else
+                {
+                    if (reference != null && reference.Count() > 0)
+                    {
+                        QueryString.AddRange(reference.Select(r => "reference[]=" + r));
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(fund))
+                    {
+                        QueryString.Add("fund=" + fund);
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(ledgerCode))
+                    {
+                        QueryString.Add("ledgerCode=" + ledgerCode);
+                    }
+
+                    if (from.HasValue)
+                    {
+                        QueryString.Add("from=" + from.Value.ToString("yyyy-MM-dd"));
+                    }
+
+                    if (to.HasValue)
+                    {
+                        QueryString.Add("to=" + to.Value.ToString("yyyy-MM-dd"));
+
+                        if (!from.HasValue)
+                        {
+                            QueryString.Add("from=" + System.Data.SqlTypes.SqlDateTime.MinValue.Value.ToString("yyyy-MM-dd"));
+                        }
+                    }
+
+                    if (methods != null && methods.Count() > 0)
+                    {
+                        QueryString.AddRange(methods.Select(r => "methods[]=" + r));
+                    }
+
+                    if (xmethods != null && xmethods.Count() > 0)
+                    {
+                        QueryString.AddRange(xmethods.Select(r => "xmethods[]=" + r));
+                    }
+                }
+
+                var response = await hc.GetAsync("v1/transactions?" + String.Join("&", QueryString));
+
+                response.EnsureSuccessStatusCode();
+
+                return await response.Content.ReadAsAsync<IEnumerable<Transaction>>();
+            }
+        }
+    }
+}
