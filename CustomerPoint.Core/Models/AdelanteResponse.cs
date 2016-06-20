bc5ea8c13a29fe6adb31acb372778298e4d68338
Ben <BforBen@users.noreply.github.com>
2016-06-20 19:34:43 +0100

    Add AdelanteResponse

---
+++ CustomerPoint.Core/Models/AdelanteResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomerPoint.Models
+{
+    public class AdelanteResponse
+    {
+        public int? ErrorStatus { get; set; }
+        public int? AuthStatus { get; set; }
+        public int? ErrorCode { get; set; }
+        public string ErrorDescription { get; set; }
+        public string IasOrderNo { get; set; }
+    }
+}
